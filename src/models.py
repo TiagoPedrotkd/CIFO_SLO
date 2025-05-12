d6eaf6d1cc734a04f599687c35e1e11114b513e7
TiagoPedrotkd <tiagopedrosoares02@gmail.com>
2025-05-12 18:06:38 +0100

    commit 5.1

--- src/models.py
+++ src/models.py
@@ -78,11 +78,11 @@ class Individual:
 
     def fitness(self):
         if not self.is_valid():
-            return -9999
+            return -1000
 
         skills = [team.average_skill() for team in self.teams]
         mean = sum(skills) / len(skills)
         variance = sum((s - mean) ** 2 for s in skills) / len(skills)
         std_dev = variance ** 0.5
 
-        return -std_dev
+        return 1000 - (std_dev * 100)
